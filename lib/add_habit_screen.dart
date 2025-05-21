893bb6a4466717b391261e30e95b8a2cc664c0d7
Rudy-X7 <rudraksh0301.g@gmail.com>
2025-05-21 17:09:24 +0530

    Update add_habit_screen.dart

--- lib/add_habit_screen.dart
+++ lib/add_habit_screen.dart
@@ -1,174 +1,28 @@
 import 'package:flutter/material.dart';
 
-class AddHabitScreen extends StatefulWidget {
-  const AddHabitScreen({super.key});
+import 'add_habit_screen.dart';
+
+class HabitTrackerScreen extends StatefulWidget {
+  final String username;
+
+  const HabitTrackerScreen({super.key, required this.username});
 
   @override
-  _AddHabitScreenState createState() => _AddHabitScreenState();
+  _HabitTrackerScreenState createState() => _HabitTrackerScreenState();
 }
 
-class _AddHabitScreenState extends State<AddHabitScreen> {
-  final TextEditingController _habitController = TextEditingController();
-  Color selectedColor = Colors.amber; // Default color
+class _HabitTrackerScreenState extends State<HabitTrackerScreen> {
   Map<String, String> selectedHabitsMap = {};
   Map<String, String> completedHabitsMap = {};
-  final Map<String, Color> _habitColors = {
-    'Amber': Colors.amber,
-    'Red Accent': Colors.redAccent,
-    'Light Blue': Colors.lightBlue,
-    'Light Green': Colors.lightGreen,
-    'Purple Accent': Colors.purpleAccent,
-    'Orange': Colors.orange,
-    'Teal': Colors.teal,
-    'Deep Purple': Colors.deepPurple,
-  };
-  String selectedColorName = 'Amber'; // Default color name
+  String name = '';
 
   @override
   void initState() {
     super.initState();
-    _loadHabits();
-  }
-
-  Future<void> _loadHabits() async {
-    setState(() {
-      // Hardcoded habits for demonstration
-      selectedHabitsMap = {
-        'Workout': 'FF5733', // Color in hex (e.g., Amber)
-        'Meditate': 'FF33A1',
-        'Read a Book': '33FFA1',
-        'Drink Water': '3380FF',
-        'Practice Gratitude': 'FFC300'
-      };
-      completedHabitsMap = {
-        'Wake Up Early': 'FF5733',
-        'Journal': 'DAF7A6'
-      };
-    });
   }
 
   Future<void> _saveHabits() async {
-    // This function intentionally left empty as no saving is needed
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // Combine both maps for display, ensuring no duplicates
-    Map<String, String> allHabitsMap = {...selectedHabitsMap, ...completedHabitsMap};
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue.shade700,
-        title: Text('Configure Habits'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: _habitController,
-              decoration: const InputDecoration(
-                labelText: 'Habit Name',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Select Color:',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 12.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.grey),
-              ),
-              child: DropdownButton<String>(
-                value: selectedColorName,
-                isExpanded: true,
-                underline: const SizedBox(),
-                items: _habitColors.keys.map((String colorName) {
-                  return DropdownMenuItem<String>(
-                    value: colorName,
-                    child: Container(
-                      height: 40,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        color: _habitColors[colorName],
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: Text(
-                        colorName,
-                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  );
-                }).toList(),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    selectedColorName = newValue!;
-                    selectedColor = _habitColors[selectedColorName]!;
-                  });
-                },
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                if (_habitController.text.isNotEmpty) {
-                  setState(() {
-                    // Add the new habit to the selectedHabitsMap with the chosen color
-                    selectedHabitsMap[_habitController.text] =
-                        selectedColor.value.toRadixString(16);
-                    _habitController.clear();
-                    selectedColorName = 'Amber'; // Reset to default
-                    selectedColor = _habitColors[selectedColorName]!;
-                  });
-                }
-              },
-              child: Text(
-                'Add Habit',
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue.shade700,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: ListView(
-                children: allHabitsMap.entries.map((entry) {
-                  final habitName = entry.key;
-                  final habitColor = _getColorFromHex(entry.value);
-                  return ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: habitColor,
-                    ),
-                    title: Text(habitName),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete, color: Colors.red),
-                      onPressed: () {
-                        setState(() {
-                          // Remove habit from both maps if it exists
-                          selectedHabitsMap.remove(habitName);
-                          completedHabitsMap.remove(habitName);
-                        });
-                      },
-                    ),
-                  );
-                }).toList(),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    //save habits to preferences in the future
   }
 
   Color _getColorFromHex(String hexColor) {
@@ -177,5 +31,194 @@ class _AddHabitScreenState extends State<AddHabitScreen> {
       hexColor = 'FF$hexColor'; // Add opacity if not included.
     }
     return Color(int.parse('0x$hexColor'));
+  }
+
+  Color _getHabitColor(String habit, Map<String, String> habitsMap) {
+    String? colorHex = habitsMap[habit];
+    if (colorHex != null) {
+      try {
+        return _getColorFromHex(colorHex);
+      } catch (e) {
+        print('Error parsing color for $habit: $e');
+      }
+    }
+    return Colors.blue; // Default color in case of error.
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade700,
+        title: Text(
+          name.isNotEmpty ? name : 'Loading...',
+          style: const TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        automaticallyImplyLeading: true,
+      ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'To Do 📝',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          selectedHabitsMap.isEmpty
+              ? const Expanded(
+                  child: Center(
+                    child: Text(
+                      'Use the + button to create some habits!',
+                      style: TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
+                  ),
+                )
+              : Expanded(
+                  child: ListView.builder(
+                    padding: const EdgeInsets.all(16.0),
+                    itemCount: selectedHabitsMap.length,
+                    itemBuilder: (context, index) {
+                      String habit = selectedHabitsMap.keys.elementAt(index);
+                      Color habitColor =
+                          _getHabitColor(habit, selectedHabitsMap);
+                      return Dismissible(
+                        key: Key(habit),
+                        direction: DismissDirection.endToStart,
+                        onDismissed: (direction) {
+                          setState(() {
+                            String color = selectedHabitsMap.remove(habit)!;
+                            completedHabitsMap[habit] = color;
+                            _saveHabits();
+                          });
+                        },
+                        background: Container(
+                          color: Colors.green,
+                          alignment: Alignment.centerRight,
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                'Swipe to Complete',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              SizedBox(width: 10),
+                              Icon(Icons.check, color: Colors.white),
+                            ],
+                          ),
+                        ),
+                        child: _buildHabitCard(habit, habitColor),
+                      );
+                    },
+                  ),
+                ),
+          const Divider(),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Done ✅🎉',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          completedHabitsMap.isEmpty
+              ? const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    'Swipe right on an activity to mark as done.',
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                )
+              : Expanded(
+                  child: ListView.builder(
+                    padding: const EdgeInsets.all(16.0),
+                    itemCount: completedHabitsMap.length,
+                    itemBuilder: (context, index) {
+                      String habit = completedHabitsMap.keys.elementAt(index);
+                      Color habitColor =
+                          _getHabitColor(habit, completedHabitsMap);
+                      return Dismissible(
+                        key: Key(habit),
+                        direction: DismissDirection.startToEnd,
+                        onDismissed: (direction) {
+                          setState(() {
+                            String color = completedHabitsMap.remove(habit)!;
+                            selectedHabitsMap[habit] = color;
+                            _saveHabits();
+                          });
+                        },
+                        background: Container(
+                          color: Colors.red,
+                          alignment: Alignment.centerLeft,
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: const Row(
+                            children: [
+                              Icon(Icons.undo, color: Colors.white),
+                              SizedBox(width: 10),
+                              Text(
+                                'Swipe to Undo',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                        child: _buildHabitCard(habit, habitColor,
+                            isCompleted: true),
+                      );
+                    },
+                  ),
+                ),
+        ],
+      ),
+      floatingActionButton: selectedHabitsMap.isEmpty
+          ? FloatingActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddHabitScreen(),
+                  ),
+                );
+              },
+              child: Icon(Icons.add),
+              backgroundColor: Colors.blue.shade700,
+              tooltip: 'Add Habits',
+            )
+          : null,
+    );
+  }
+
+  Widget _buildHabitCard(String title, Color color,
+      {bool isCompleted = false}) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      color: color,
+      child: Container(
+        height: 60, // Adjust the height for thicker cards.
+        child: ListTile(
+          title: Text(
+            title.toUpperCase(),
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          trailing: isCompleted
+              ? const Icon(Icons.check_circle, color: Colors.green, size: 28)
+              : null,
+        ),
+      ),
+    );
   }
 }
